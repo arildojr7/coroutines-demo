324fe4829b4db81f5acfd76cfd8b1047cafc9e2e
Arildo Borges Jr <sitesabj@gmail.com>
2020-04-24 00:28:27 -0300

    add more complex animations in `AnimationsActivity`; implements `View` extensions to help animation; add some resources

--- app/src/main/java/dev/arildo/coroutinesdemo/menu/activity/MenuActivity.kt
+++ app/src/main/java/dev/arildo/coroutinesdemo/menu/activity/MenuActivity.kt
@@ -6,6 +6,7 @@ import dev.arildo.coroutinesdemo.R
 import dev.arildo.coroutinesdemo.animations.activity.AnimationsActivity
 import dev.arildo.coroutinesdemo.core.base.BaseActivity
 import dev.arildo.coroutinesdemo.databinding.ActivityMenuBinding
+import dev.arildo.coroutinesdemo.multiplerequests.activity.MultipleRequestsActivity
 import dev.arildo.coroutinesdemo.singlerequest.activity.SingleRequestActivity
 
 class MenuActivity : BaseActivity<ActivityMenuBinding>(R.layout.activity_menu) {
@@ -27,7 +28,7 @@ class MenuActivity : BaseActivity<ActivityMenuBinding>(R.layout.activity_menu) {
             startActivity(Intent(this, SingleRequestActivity::class.java))
         }
         binding.btnMultipleRequests.setOnClickListener {
-            startActivity(Intent(this, AnimationsActivity::class.java))
+            startActivity(Intent(this, MultipleRequestsActivity::class.java))
         }
     }
 }
